adcdfe699b1f1d605a005f8a3cb5cf700c0791ab
darbyxa <119996833+darbyxa@users.noreply.github.com>
2025-03-12 11:24:11 +0000

    Added inverted integers implementation

--- InvertedInteger.py
+++ InvertedInteger.py
@@ -1,1 +1,17 @@
+class InvertedInteger:
 
+
+      def __init__(self, obj):
+            self.object = obj
+
+
+      # overwrite "print"
+      def __str__(self):
+            return "<" + str(self.object) + ">"
+
+
+      # define "*"
+      def __mul__(self,other):
+            return InvertedInteger(self.object
+                                              + other.object
+                                              + self.object*other.object)
